f096e27c2e5a3e33cbc659d8b6d2cf5eeec69e0d
hhashimoto <almite.bridge@gmail.com>
2018-06-10 20:58:26 +0900

    Add registration view

--- app/views.py
+++ app/views.py
@@ -3,7 +3,12 @@ from django.http import HttpResponse
 
 from .models import Item
 
+
 def index(request):
     items = Item.objects.order_by('-created_at')
     resp_text = '<br>'.join([item.name for item in items])
     return HttpResponse(resp_text)
+
+
+def register(request):
+    return render(request, 'register.html', {})
