bc37b331c59fc4087bd28de4041207f2653ed830
hhashimoto <almite.bridge@gmail.com>
2018-06-10 18:24:51 +0900

    Listing game title

--- app/views.py
+++ app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Item
+
 def index(request):
-    return HttpResponse('index page')
+    items = Item.objects.order_by('-created_at')
+    resp_text = '<br>'.join([item.name for item in items])
+    return HttpResponse(resp_text)
