ba4270e7fdfe93baf8fff2110f76a5bb0c178a95
hhashimoto <almite.bridge@gmail.com>
2018-06-10 18:07:11 +0900

    Add game item model #1

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
